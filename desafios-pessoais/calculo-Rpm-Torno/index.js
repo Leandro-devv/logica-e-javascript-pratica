92c97ad3684223c65f76e9e0bd6c3c12d6115f0e
leandro_caf2@hotmail.com <leandro_caf2@hotmail.com>
2026-02-04 14:23:11 -0300

    implementando logica de calculo de RPM com validacao

--- desafios-pessoais/calculo-Rpm-Torno/index.js
+++ desafios-pessoais/calculo-Rpm-Torno/index.js
@@ -1,9 +1,17 @@
 
 function calcularRpm() {
-    let velocidadeCorte = document.getElementById("velocidade").value;
-    let diametro = document.getElementById("diametro").value;
-    let resultado = document.getElementById("resultado");
-    let rpm = (1000 * velocidadeCorte) / (3.14 * diametro);
-    resultado.innerHTML = "O valor do RPM é: " + rpm.toFixed(2);
-    return rpm.toFixed(2);
+    const velocidadeCorte = document.getElementById("velocidade")
+    const diametro = document.getElementById("diametro")
+
+    const v = parseFloat(velocidadeCorte.value);
+    const d = parseFloat(diametro.value);
+
+    const resultado = document.getElementById("resultado");
+
+    if(v>0 && d>0){
+        const rpm = (1000 * v) / (3.14 * d)
+        resultado.innerHTML = `O RPM do torno é: ${rpm.toFixed(2)} rpm`;
+    }else{
+        resultado.innerHTML = "Por favor, insira valores válidos para velocidade de corte e diâmetro.";
+    }
 }
