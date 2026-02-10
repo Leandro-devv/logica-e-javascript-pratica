66c09a39e9c8e01748346da9427c73cf76d4bca6
leandro_caf2@hotmail.com <leandro_caf2@hotmail.com>
2026-02-10 16:57:39 -0300

    implementando galeria de EPIs com alinhamento flexbox e correções de ID

--- curso-guanabara/projeto-segurança/index.js
+++ curso-guanabara/projeto-segurança/index.js
@@ -15,16 +15,28 @@ function identificador(){
     }else{
         let texto = document.createElement('h3');
         let recomendacao = document.createElement('p');
-        let imagem = document.createElement('img');
-        imagem.setAttribute('id','imagem')
-        recomendacao.setAttribute('id','recomendação')
+        recomendacao.setAttribute('id','recomendação');
+        
+        let imagem_1 = document.createElement('img');
+        imagem_1.setAttribute('class','icone');
+        let imagem_2 = document.createElement('img');
+        imagem_2.setAttribute('class','icone');
+        let imagem_3 = document.createElement('img');
+        imagem_3.setAttribute('class','icone');
+        
+           
+        let containerEpi = document.createElement('div')
+        containerEpi.setAttribute('id', 'containerEpi')
 
         texto.innerHTML = `olá ${nome}, voce faz parte do setor: ${setor}`;
         res.appendChild(texto);
 
        
         if(setor == 'administrativo'){
-            recomendacao.innerHTML = `Atenção você deve usar os EPIs se for entrar na fábrica!`; 
+            recomendacao.innerHTML = `Atenção, você tem a opção de usar os EPIs somente ao entrar na fábrica!`;
+            imagem_1.setAttribute('src','imagens/icones/bota.jpg');
+            imagem_2.setAttribute('src','imagens/icones/auricular.jfif');
+            imagem_3.setAttribute('src','imagens/icones/oculos.png');       
         }else if(setor == 'operacional' ){
 
         }else if(setor == 'Manutenção' ){
@@ -32,8 +44,13 @@ function identificador(){
         }else if(setor == 'Segurança'){
 
         }
+
+        containerEpi.appendChild(imagem_1);
+        containerEpi.appendChild(imagem_2);
+        containerEpi.appendChild(imagem_3);
         
         res.appendChild(recomendacao);
+        res.appendChild(containerEpi);
     }
 
 }
